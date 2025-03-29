7086c39f2237046f223af2628aa1be08646b0a6c
HYT <hunteryang0108@gmail.com>
2025-03-30 05:10:19 +0800

    Searchbar prototype

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,25 @@
-<script setup>
+<script>
   import MapCanvas from './components/MapCanvas.vue';
+  import SearchBar from './components/SearchBar.vue';
+  export default {
+    components: {
+      MapCanvas,
+      SearchBar,
+    },
+    methods: {
+      toggleSearchbar() {
+        this.$refs.SearchBar.searchbarVisible = !this.$refs.SearchBar.searchbarVisible;
+      },
+      showResult(data) {
+        //this.$refs.MapCanvas.showResult(data);
+      },
+    },
+    provide() {
+      return {
+        showResult: this.showResult,
+      }
+    },
+  }
 </script>
 
 <template>
@@ -9,9 +29,10 @@
         <a href="/" class="fs-2 text-black fw-bold m-0 px-2 text-decoration-none">NCU Map</a>
         <button class="btn btn-link" @click="toggleSearchbar"><i class="bi-search fs-5 text-black"></i></button>
       </div>
+      <SearchBar ref="SearchBar" />
     </header>
     <main class="d-flex flex-grow-1 justify-content-center">
-      <MapCanvas />
+      <MapCanvas ref="MapCanvas" />
     </main>
     <footer class="bg-dark text-white text-center py-3">
       <p class="m-0">Copyright © 2025 NCU Map</p>
