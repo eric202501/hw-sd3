cbd7767ecda00e0dbd6d71c7bfd980f2a5491920
Hunter <hunteryang0108@gmail.com>
2025-03-29 21:01:41 +0800

    Fix phone view problem

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,26 @@
 <script setup>
-import MapCanvas from './components/MapCanvas.vue'
+  import MapCanvas from './components/MapCanvas.vue'
 </script>
 
 <template>
-  <div class="d-flex flex-column min-vh-100">
+  <div id="app" class="d-flex flex-column">
     <header class="bg-light shadow py-3">
       <div class="container d-flex justify-content-between align-items-center">
-        <h2 class="fw-bold m-0">NCU Map</h2>
+        <h2 class="fw-bold m-0 px-2">NCU Map</h2>
         <button class="btn btn-link"><i class="bi bi-search fs-5 text-black"></i></button>
       </div>
     </header>
-
-    <main class="container d-flex flex-grow-1 justify-content-center align-items-center my-5">
-      <div class="d-flex rounded-4 overflow-hidden">
-        <MapCanvas />
-      </div>
+    <main class="d-flex flex-grow-1 justify-content-center align-items-center">
+      <MapCanvas />
     </main>
-
-    <footer class="bg-dark text-white text-center mt-auto py-3">
+    <footer class="bg-dark text-white text-center py-3">
       <p class="m-0">Copyright &copy; 2025 NCU Map</p>
     </footer>
   </div>
 </template>
+
+<style>
+  html, body, #app {
+    height: 100%;
+  }
+</style>
