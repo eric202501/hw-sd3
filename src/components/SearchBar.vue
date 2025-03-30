83ac652fd09930b69ce794cfbfa19c207d08b8f8
eric202501 <eric202502@gmail.com>
2025-03-31 00:17:09 +0800

    hover and tooltip

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -33,6 +33,7 @@
   .search-input {
     box-shadow: 0 -1rem 2rem 2rem rgba(0, 0, 0, .3);
     clip-path: inset(-0.5rem -5rem -5rem -5rem);
+    z-index: 100;
   }
   .searchbar-enter-active, .searchbar-leave-active {
     transition: max-height 0.15s ease-out, opacity 0.15s ease-out;
